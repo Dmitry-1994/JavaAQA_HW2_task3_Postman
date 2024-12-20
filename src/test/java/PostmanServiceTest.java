59655c48033560997cfbc23098a4614a51f73e02
Dmitry Tarasov <vizalyyy@gmail.com>
2024-12-21 00:35:10 +0500

    Initial commit

---
+++ src/test/java/PostmanServiceTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class PostmanServiceTest {
+
+    @Test
+    public void shouldReturnData(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Меня зовут Дмитрий") // отправляемые данные (заголовки и query можно выставлять аналогично)
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Меня зовут Дмитрий"))
+        ;
+    }
+
+
+}
